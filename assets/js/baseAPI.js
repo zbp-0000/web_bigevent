ebff90029a51e1b35405c720a88a80499639652f
zbp_02 <zbp_02@qq.com>
2022-06-17 21:18:36 +0800

    完成个人中心开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,7 +3,7 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url 
-    console.log(options.url);
+    // console.log(options.url);
     // 统一为有权限的接口，设置 headers 请求头
     if(options.url.indexOf('/my/') !== -1) {
         options.headers = {
